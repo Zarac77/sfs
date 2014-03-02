0ce52e59776da48f36285ad00c35a723718347df
radgeRayden <westerbly@gmail.com>
2014-03-02 04:07:16 -0300

    Incluindo arquivos que faltaram no commit anterior

---
+++ Sfs/Sfs/ViewModels/MatrizViewModels/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Sfs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sfs.ViewModels.MatrizViewModels {
+    public class IndexViewModel : ListaPaginada<Pessoa> {
+        public Dictionary<Guid, int> SaidasPessoa { get; set; }
+
+        public IndexViewModel() {
+            SaidasPessoa = new Dictionary<Guid, int>();
+        }
+    }
+}
